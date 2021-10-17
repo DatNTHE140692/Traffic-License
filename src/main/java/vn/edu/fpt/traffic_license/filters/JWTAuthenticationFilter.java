c6cbe81a80fae806f70f2bb3fc375fea80fae17b
datnt368 <datnt368@ghtk.co>
2021-10-17 22:47:59 +0700

    Refactor Code

--- src/main/java/vn/edu/fpt/traffic_license/filters/JWTAuthenticationFilter.java
+++ src/main/java/vn/edu/fpt/traffic_license/filters/JWTAuthenticationFilter.java
@@ -1,8 +1,11 @@
 package vn.edu.fpt.traffic_license.filters;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -10,6 +13,8 @@ import org.springframework.security.web.authentication.WebAuthenticationDetailsS
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 import vn.edu.fpt.traffic_license.config.jwt.JWTConfig;
+import vn.edu.fpt.traffic_license.constants.ResponseStatusCodeConst;
+import vn.edu.fpt.traffic_license.response.ResponseFactory;
 import vn.edu.fpt.traffic_license.service.UserServices;
 import vn.edu.fpt.traffic_license.utils.StringUtils;
 import vn.edu.fpt.traffic_license.utils.jwt.JWTUtils;
@@ -19,6 +24,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.nio.charset.StandardCharsets;
 
 @Slf4j
 @Component
@@ -27,46 +33,55 @@ public class JWTAuthenticationFilter extends OncePerRequestFilter {
     private final JWTConfig jwtConfig;
     private final JWTUtils jwtUtils;
     private final UserServices userServices;
+    private final ResponseFactory responseFactory;
 
     @Autowired
     public JWTAuthenticationFilter(JWTConfig jwtConfig,
                                    JWTUtils jwtUtils,
-                                   UserServices userServices) {
+                                   UserServices userServices,
+                                   ResponseFactory responseFactory) {
         this.jwtConfig = jwtConfig;
         this.jwtUtils = jwtUtils;
         this.userServices = userServices;
+        this.responseFactory = responseFactory;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         try {
             String authorization = request.getHeader(HttpHeaders.AUTHORIZATION);
-            if (StringUtils.isNotBlank(authorization) && authorization.startsWith(jwtConfig.getTokenPrefix())) {
-                String token = authorization.substring(7);
-                String username = jwtUtils.extractUsername(token);
-                if (jwtUtils.isTokenValid(token) && StringUtils.isNotBlank(username)) {
-                    UserDetails userDetails = userServices.loadUserByUsername(username);
-                    if (userDetails != null) {
-                        UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
-                                userDetails,
-                                null,
-                                userDetails.getAuthorities()
-                        );
-                        authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-                        SecurityContextHolder.getContext().setAuthentication(authentication);
-                    } else {
-                        response.sendError(HttpServletResponse.SC_FORBIDDEN);
-                        return;
-                    }
-                }
-            } else {
-                response.sendError(HttpServletResponse.SC_FORBIDDEN);
+            if (StringUtils.isBlank(authorization) || !authorization.startsWith(jwtConfig.getTokenPrefix())) {
+                returnError(response, ResponseStatusCodeConst.FORBIDDEN, null);
                 return;
+            }
+            String token = authorization.substring(7);
+            String username = jwtUtils.extractUsername(token);
+            if (jwtUtils.isTokenValid(token) && StringUtils.isNotBlank(username)) {
+                UserDetails userDetails = userServices.loadUserByUsername(username);
+                if (userDetails == null) {
+                    returnError(response, ResponseStatusCodeConst.FORBIDDEN, null);
+                    return;
+                }
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,
+                        null,
+                        userDetails.getAuthorities()
+                );
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
             }
         } catch (Exception ex) {
             log.error("Authentication Failed, {}", ex.getMessage());
         }
         filterChain.doFilter(request, response);
+    }
+
+    public void returnError(HttpServletResponse response, ResponseStatusCodeConst statusCodeConst, String errorDetail) throws IOException {
+        ResponseEntity<Object> responseEntity = responseFactory.fail(errorDetail, statusCodeConst);
+        response.setStatus(statusCodeConst.getHttpCode());
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name());
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+        new ObjectMapper().writeValue(response.getOutputStream(), responseEntity.getBody());
     }
 
 }
